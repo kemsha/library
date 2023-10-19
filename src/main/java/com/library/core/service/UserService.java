172d19f9fb778aa60e29850784a28c3e0b0f668c
kemsha <arijan.komsic@stu.ibu.edu.ba>
2023-10-19 19:17:23 +0200

    user service and controller updated

--- src/main/java/com/library/core/service/UserService.java
+++ src/main/java/com/library/core/service/UserService.java
@@ -51,4 +51,27 @@ public class UserService {
         User user = userRepository.save(payload.toEntity());
         return new UserDTO(user);
     }
+
+    public UserDTO updateUser(String id, UserRequestDTO payload){
+        Optional<User> user = userRepository.findById(id);
+
+        if(user.isEmpty()){
+            throw new ResourceNotFoundException("The user with given ID does not exist.");
+        }
+        User updatedUser = payload.toEntity();
+        updatedUser.setId(user.get().getId());
+        updatedUser = userRepository.save(updatedUser);
+        return new UserDTO(updatedUser);
+    }
+
+    public void deleteUser(String id) {
+        Optional<User> user = userRepository.findById(id);
+        user.ifPresent(userRepository::delete);
+    }
+
+    public UserDTO filterByEmail(String email) {
+        Optional<User> user = userRepository.findByFirstByEmailLike(email);
+
+        return user.map(UserDTO::new).orElse(null);
+    }
 }
--- src/main/java/com/library/rest/controllers/UserController.java
+++ src/main/java/com/library/rest/controllers/UserController.java
@@ -1,4 +1,53 @@
 package com.library.rest.controllers;
 
+import com.library.core.service.UserService;
+import com.library.rest.dto.UserDTO;
+import com.library.rest.dto.UserRequestDTO;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/")
+    public ResponseEntity<List<UserDTO>> getUsers() {
+        return ResponseEntity.ok(userService.getUsers());
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/register")
+    public ResponseEntity<UserDTO> register(@RequestBody UserRequestDTO user) {
+        return ResponseEntity.ok(userService.addUser(user));
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable String id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, path = "/{id}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable String id, @RequestBody UserRequestDTO user) {
+        return ResponseEntity.ok(userService.updateUser(id, user));
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, path = "/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable String id) {
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/filter")
+    public ResponseEntity<UserDTO> filterUser(@RequestParam String email) {
+        return ResponseEntity.ok(userService.filterByEmail(email));
+    }
 }
